8235b782dffd507716a020fff2577caba15b66af
Iqball... <ramaiqbal2007@gmail.com>
2025-12-02 08:42:07 +0700

    Gratitude

---
+++ Gratitude_17.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Gratitude_17 {
+
+    public static void sayThankyou() {
+        System.out.println("Thank you for everything!");
+    }
+
+    public static void main(String[] args) {
+
+        sayThankyou();
+        System.out.println("Thank you for supporting me!");
+    }
+}
